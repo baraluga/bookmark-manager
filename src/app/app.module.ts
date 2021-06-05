efd33f8af750371034144e92a3b2de5a516db0d8
Brian Alexander <brian.peralta@engie.com>
2021-06-05 22:41:53 +0800

    Installed @angular/material;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { bookmarksReducer, BOOKMARKS_STATE_KEY } from './store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     StoreModule.forRoot({ [BOOKMARKS_STATE_KEY]: bookmarksReducer }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
